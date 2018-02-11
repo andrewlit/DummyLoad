a851d37099891527233815de56e6515e992c50cc
Andrew Lit <andrew@andrewlit.com>
2018-02-11 00:55:20 -0500

    Eet iz vurkink! Vastly simplified the system
    Changed the display manager to a simple array based display system.
    Implemented the front panel button to disable the buzzer. Still need to
    implement the actual operation part, but interface wise it's mostly
    done.

--- code/LoadCodeV1/LoadCodeV1.ino
+++ code/LoadCodeV1/LoadCodeV1.ino
@@ -13,7 +13,7 @@ xx.xx W  Logging
  
  */
 #include"LiquidCrystal595.h"
-#include"DisplayManager.h"
+//#include"DisplayManager.h"
 
 //Pin Assignments
 //DAC Shift Register; 16 bit serial
@@ -31,21 +31,44 @@ const int DISP_d = 8;
 const int ENC_1 = 14;
 const int ENC_3 = 13;
 const int ENC_button = 9;
-int ENC_LAST = 0;
+bool ENC_LAST = 0;
 
+const int RES_SW = 12;
 const int BUZZER = 10;
+bool BUZZER_ON = 1;
 
 const int VIN = 5;
 
+//Struct for a basic on-screen parameter
+struct parameter{
+	byte x, y, dec, inc;
+	unsigned short value;
+} voltage, current, power, cutoff, enabled;
 
 
-  unsigned short num1 = 32;
-  unsigned short num2 = 60;
-  unsigned short num3 = 3;
+
+char DISP_OFF[4][21]=  {"Set:      A     load",
+			"Cut:      V      off",	
+			"Vlt:      V         ",
+			"Pwr:      W         "};
+
+
+char DISP_ON [4][21]=  {"Set:      A     load",
+			"Cut:      V       on",	
+			"Vlt:      V         ",
+			"Pwr:      W        *"};
+
+//Array of editable parameters
+parameter* editParams[] {&current, &cutoff, &enabled};
+
+//number of parameters to loop through
+byte NUM_PARAMS = 2;
+
+
 LiquidCrystal595 lcd ( DISP_d, DISP_l, DISP_c);
 
 
-  DisplayManager dispman (&lcd);
+ // DisplayManager dispman (&lcd);
 void setup() {
 
   pinMode(V_in, INPUT);
@@ -56,40 +79,64 @@ void setup() {
   pinMode (ENC_1, INPUT_PULLUP);
   pinMode (ENC_3, INPUT_PULLUP);
   pinMode (ENC_button, INPUT_PULLUP);
+  pinMode (RES_SW, INPUT_PULLUP);
   dacOut(0);
+
 
   digitalWrite(BUZZER, LOW);
   lcd.init();
 
-  uint8_t cols = 20;
-  uint8_t rows = 4;
 
-  lcd.begin(cols, rows);
+  lcd.begin(20, 4);
   lcd.home();
-  lcd.print(0);
 
-  //char name[] = "na1:";
+  //Set up the parameter structs
+  voltage.x = 4;
+  voltage.y = 2;
+  voltage.dec = 2;
+  voltage.inc = 1;
+  voltage.value = 0;
 
-  dispman.addObj(0,0, "hi",  &num1, 0, 2);
-  //dispman.addObj(5,1, "Thing 2:",  &num2, 1, 3);
-  //dispman.addObj(2,2, "Thing 3:", &num2, 0, 2);
-  //dispman.addObj(0,3, "Thing 4:", &num1, 1,2);
+  current.x = 4;
+  current.y = 0;
+  current.dec=3;
+  current.inc = 10;
+  current.value=200;
 
-  //dispman.update();
-  dispman.show();
-  lcd.cursor();
-  //dispman.objAppendCursor();
-  flash(2);
+  cutoff.x = 4;
+  cutoff.y = 1;
+  cutoff.inc = 1;
+  cutoff.dec = 2;
+  cutoff.value = 500;
+
+  power.x=4;
+  power.y=3;
+  power.dec=2;
+  power.inc=NULL;
+  power.value=0;
+
+  enabled.x=16;
+  enabled.y=1;
+  enabled.value=0;
+
+
+  putDisplay(DISP_OFF);
   delay(1000);
+  updateParams();
+  showParam(&voltage);
+  showParam(&current);
+  showParam(&cutoff);
+  showParam(&power);
+  delay(1000);
+  lcd.cursor();
 }
 
-short inc = 1;
 bool MODE = 0;
-
 bool n = 0;
+short CURRENT_PARAM = 0;
 
 void loop() {
-  
+
   if (digitalRead(ENC_1) != n)
   {
       ENC_LAST = n;
@@ -101,38 +148,131 @@ void loop() {
     ENC_LAST = n; // Do not execute this statement again until the encoder changes
     if (digitalRead(ENC_3) == LOW)
     {
-      if (MODE) dispman.objIncrementVal(1);
+      if (MODE)
+      {
+	      if(editParams[CURRENT_PARAM]==&enabled)
+	      {
+	      enabled.value = !enabled.value;
+	      beep(100);
+		      redraw();
+		      putCursor();
+	      }
+      
+	      else{
+	      editParams[CURRENT_PARAM]->value+= editParams[CURRENT_PARAM]->inc;	      		
+	      showParam(editParams[CURRENT_PARAM]);
+	      }
+      }
+      
       else 
       {
-        dispman.nextObjCheck();
-        dispman.objAppendCursor();
+	      if (CURRENT_PARAM == NUM_PARAMS) CURRENT_PARAM=0;
+	      else CURRENT_PARAM ++;
+	      lcd.setCursor(editParams[CURRENT_PARAM]->x,editParams[CURRENT_PARAM]->y);
       }
     }
+    
     else
     {
-      if (MODE) dispman.objIncrementVal(-1);
+      if (MODE)
+      {
+	      if(editParams[CURRENT_PARAM]==&enabled)
+	      {
+		      enabled.value = !enabled.value;
+		      beep(100);
+	      	redraw();
+		putCursor();
+	     }
+	      else{
+	      editParams[CURRENT_PARAM]->value-=editParams[CURRENT_PARAM]->inc;
+	      showParam(editParams[CURRENT_PARAM]);
+	      }
+      }
+     
+    
       else
       {
-        dispman.prevObjCheck();
-        dispman.objAppendCursor();
+	      if (CURRENT_PARAM == 0) CURRENT_PARAM=NUM_PARAMS;
+	      else CURRENT_PARAM --;
+	      lcd.setCursor(editParams[CURRENT_PARAM]->x,editParams[CURRENT_PARAM]->y);
       }
     }
-    if(MODE) dispman.objAppendParam();
   }
+  
+
   if (digitalRead(ENC_button) == LOW) 
   {
     MODE = !MODE;
     if (MODE) lcd.blink();
-    else {
-	    lcd.noBlink(); 
-	    updateParams();
-	    dispman.update();
-    }
+    else lcd.noBlink(); 
+    updateParams();
     delay(200);
   }
- 
+
+  if (digitalRead(RES_SW) == LOW)
+  {
+	  BUZZER_ON = !BUZZER_ON;
+	  delay(300);
+    }
 }
-void flash(byte pin)
+
+void putDisplay(char data[4][21])
+{
+	for (int y = 0; y < 4; y++)
+	{
+		lcd.setCursor(0,y);
+		lcd.print(data[y]);
+	}
+}
+
+void showParam(parameter *p)
+{
+	lcd.setCursor(p->x, p->y);
+	String s = String(p->value);
+
+	//Pad with zeroes if needed
+	s = String("0000").substring(0, 4-s.length())+s;
+//	if (s.length()-p->dec == 2) s = "00" + s;
+//	else if (s.length()-p->dec == 1) s="0" + s;
+
+	//Add the decimal point
+	s= s.substring(0, s.length()-p->dec) + "." + s.substring(s.length()-p->dec);
+	
+	lcd.print(s);
+}
+
+void updateParams()
+{
+	power.value = (current.value/100)*(cutoff.value/10);
+	showParam(&power);
+	putCursor();
+}
+
+void redraw()
+{
+	if (enabled.value)
+		putDisplay(DISP_ON);
+	else putDisplay(DISP_OFF);
+	updateParams();
+
+  showParam(&voltage);
+  showParam(&current);
+  showParam(&cutoff);
+  showParam(&power);
+}
+
+
+void putCursor()
+{
+	lcd.setCursor(editParams[CURRENT_PARAM]->x,editParams[CURRENT_PARAM]->y);
+}
+
+void putCursor(parameter* p)
+{
+	lcd.setCursor(p->x,p->y);
+}
+
+void flash(int pin)
 {
   digitalWrite(pin, HIGH);
   delay (500);
@@ -150,20 +290,14 @@ void dacOut(int val)
 
 //Turns out the buzzer actually has an internal oscillator, so all we have to
 // do is pull the pin high
-void beep(unsigned short duration)
+void beep(int duration)
 {
+	if(!BUZZER_ON) return;
+
   digitalWrite(BUZZER, HIGH);
   delay(duration);
   digitalWrite(BUZZER, LOW);
 }
-
-//Update and dependent parameters
-void updateParams()
-{
-	num2 = 15*num1;
-}
-
-
 
 
 
